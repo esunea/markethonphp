0bbb2bf5c069a59cfd12695442ab2782c892d8e6
Léo DESIGAUX <leodesigaux@gmail.com>
2018-04-16 00:00:56 +0200

    Base design

--- Class/Page.class.php
+++ Class/Page.class.php
@@ -13,15 +13,24 @@ class Page {
 		<head>
 		<meta charset='UTF-8'>
 		<title>Markethon</title>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.0/css/bulma.min.css' />
 		</head>
 		<body>
-		<h1>PAGE</h1>
+      <section class='hero is-info is-bold'>
+        <div class='hero-body' style='padding: 1rem 1.5rem;'>
+          <div class='container'>
+            <h1 class='title'>Markethon 2018</h1>
+          </div>
+        </div>
+      </section>
+
 		";
 		$this->renderMenu();
 	}
 	function __destruct(){
 
 		$this->content .="
+    </div>
 		</body>
 		</html>
 		";
@@ -31,7 +40,7 @@ class Page {
 	// offre *******************************************************
 
 
-	
+
 	function renderOffreUpdate($listOfEntreprises, $offre){
 		// $this->content .="
 		// <form action='' method='post'>
@@ -41,9 +50,13 @@ class Page {
 		// ";
 
 		$this->content .="
-		<form action='' method='post'>
-		<select name='idEntreprise'>
-		<option value='-1'>None</option>
+    <form action='' method='post' id='form'>
+    <div class='field'>
+      <label class='label'>Entreprise</label>
+      <div class='control'>
+        <div class='select'>
+          <select name='idEntreprise'>
+		        <option value='-1'>None</option>
 		";
 
 		foreach ($listOfEntreprises as $id => $name) {
@@ -59,16 +72,29 @@ class Page {
 			}
 		}
 		$this->content .="
-		</select>
-		<input type='text' name='name' value='".$offre->getName()."'>
-		<input type='submit' name='OK'>
+		      </select>
+        </div>
+      </div>
+    </div>
+    <div class='field'>
+      <label class='label'>Nom de l'offre</label>
+      <div class='control'>
+		    <input class='input' type='text' name='name' value='".$offre->getName()."'>
+      </div>
+    </div>
+		<input type='submit' name='OK' class='button is-link'>
 		</form>
 		";
 	}
 	function renderOffreForm($array){
 		$this->content .="
-		<form action='' method='post'>
-		<select name='idEntreprise'>";
+		<form action='' method='post' id='form'>
+    <div class='field'>
+      <label class='label'>Entreprise</label>
+      <div class='control'>
+        <div class='select'>
+          <select name='idEntreprise'>
+            <option value='-1'>None</option>";
 
 		foreach ($array as $key => $value) {
 			$this->content .="
@@ -76,49 +102,49 @@ class Page {
 			";
 		}
 		$this->content .="
-		</select>
-		<input type='text' name='name'>
-		<input type='submit' name='OK'>
+		      </select>
+        </div>
+      </div>
+    </div>
+    <div class='field'>
+      <label class='label'>Nom de l'offre</label>
+      <div class='control'>
+        <input class='input' type='text' name='name'>
+      </div>
+    </div>
+    <input type='submit' name='OK' class='button is-link'>
 		</form>
 		";
 	}
 	function renderOffreSucces($id=0){
-		$this->content .="
-		<p>l'offre a été correctement ajoutée</p>
-		";
-		if($id==1)$this->content .= "(modifications)";
+    if ($id==1) {
+      $this->content .= $this->renderNotification("L'offre a été correctement modifiée", "success");
+    } else {
+      $this->content .= $this->renderNotification("L'offre a été correctement ajoutée", "success");
+    }
 	}
 	function renderOffreFail($id=0){
-		$this->content .="
-		<p>l'offre n'as pas été ajoutée</p>
-		";
-		if($id==1)$this->content .= "(modifications)";
+    if ($id==1) {
+      $this->content .= $this->renderNotification("L'offre n'a pas été modifiée", "danger");
+    } else {
+      $this->content .= $this->renderNotification("L'offre n'a pas été ajoutée", "danger");
+    }
 	}
 	function renderOffreDetail($offre){
 		$this->content .="
-		<section>
-		<h1>offre: ".$offre->getName()."</h1>
-		<p>entreprise: ".$offre->getEntrepriseName()."</p>
-		<a href='edit_offre.php?id=".$offre->getId()."'>Edit</a>
+		<section class='content'>
+		<h1>Offre: ".$offre->getName()."</h1>
+		<p><strong>Entreprise:</strong> ".$offre->getEntrepriseName()."</p>
+    <a class='button is-info' href='edit_offre.php?id=".$offre->getId()."'>Edit</a>
 		</section>
 		";
 	}
 	function renderOffreNotFound(){
-		$this->content .= "
-		<p>Cette offre n'existe pas </p>
-		";
+    $this->content .= $this->renderNotification("Cette offre n'existe pas", "warning");
 	}
 	function renderConfirmDeleteOffre(){
-		$this->content .="
-		<p>L'offre a bien été supprimée</p>
-		";	
+    $this->content .= $this->renderNotification("L'offre a été correctement supprimée", "success");
 	}
-
-
-
-
-
-
 
 
 
@@ -130,74 +156,95 @@ class Page {
 		$this->content .="
 		<form action='' method='post'>
 		<input type='hidden' name='id' value=".$entreprise->getId().">
-		<input type='text' name='name' value='".$entreprise->getName()."'>
-		<input type='submit' name='OK'>
+    <div class='field'>
+      <label class='label'>Nom de l'entreprise</label>
+      <div class='control'>
+        <input class='input' type='text' name='name' value='".$entreprise->getName()."'>
+      </div>
+    </div>
+    <input type='submit' name='OK' class='button is-link'>
+    <a class='button is-danger' href='delete_entreprise.php?id=".$entreprise->getId()."'>Delete</a>
 		</form>
-		<a href='delete_entreprise.php?id=".$entreprise->getId()."'>Delete</a>
 		";
 	}
 	function renderEntrepriseForm(){
 		$this->content .="
-		<form action='' method='post'>
-		<input type='text' name='name'>
-		<input type='submit' name='OK'>
+    <form action='' method='post' id='form'>
+		<div class='field'>
+      <label class='label'>Nom de l'entreprise</label>
+      <div class='control'>
+        <input class='input' type='text' name='name'>
+      </div>
+    </div>
+    <input type='submit' name='OK' class='button is-link'>
 		</form>
 		";
 	}
 	function renderEntrepriseSucces($id=0){
-		$this->content .="
-		<p>l'entreprise a été correctement ajoutée</p>
-		";
-		if($id==1)$this->content .= "(modifications)";
+    if ($id==1) {
+      $this->content .= $this->renderNotification("L'entreprise a été correctement modifiée", "success");
+    } else {
+      $this->content .= $this->renderNotification("L'entreprise a été correctement ajoutée", "success");
+    }
 	}
 	function renderEntrepriseFail($id=0){
-		$this->content .="
-		<p>l'entreprise n'as pas été ajoutée</p>
-		";
-		if($id==1)$this->content .= "(modifications)";
+    if ($id==1) {
+      $this->content .= $this->renderNotification("L'entreprise n'as pas été modifiée");
+    } else {
+      $this->content .= $this->renderNotification("L'entreprise n'as pas été ajoutée");
+    }
 	}
-	
-	
+
+
 
 	function renderEntrepriseDetail($entreprise){
 		$this->content .="
-		<section>
-		<h1>Entreprise: ".$entreprise->getName()."</h1>
-		<a href='edit_entreprise.php?id=".$entreprise->getId()."'>Edit</a>
+		<section class='content'>
+		<h1>Entreprise: ".$entreprise->getName()."</h1><br/>
+		<a class='button is-info' href='edit_entreprise.php?id=".$entreprise->getId()."'>Edit</a>
 		</section>
 		";
 	}
-	
+
 	function renderEntrepriseNotFound(){
-		$this->content .= "
-		<p>Cette entreprise n'existe pas </p>
-		";
+    $this->content .= $this->renderNotification("Cette entreprise n'existe pas", "warning");
 	}
 
 
 
 	function renderConfirmDeleteEntreprise(){
-		$this->content .="
-		<p>L'entreprise a bien été supprimée</p>
-		";	
+		$this->content .= $this->renderNotification("L'entreprise a bien été supprimée", "success");
 	}
 
 
 	function renderNotFound(){
-		$this->content .="
-		<p>Stop asking for this page or you'll create a f**kin space-time rift that will annihilate the  human race and maybe the whole universe</p>
-		";
+		$this->content .= $this->renderNotification("Stop asking for this page or you'll create a f**kin space-time rift that will annihilate the  human race and maybe the whole universe");
 	}
-	function renderMenu(){
-		$this->content .="
-		<nav>
-		<a href='ajout_entreprise.php'> Ajout entreprise</a>
-		<a href='ajout_offre.php'> Ajout offre</a>
-		<a href='affiche_offre.php?id=1'> Affiche offre</a>
-		<a href='affiche_entreprise.php?id=1'> Affiche entreprise</a>
-		</nav>
-		";
-	}
+  function renderNotification($message, $type='danger'){
+    // types: danger, success, warning, info
+    return "<div class='notification is-".$type."'>".$message."</div>";
+  }
+
+  function renderMenu(){
+    $this->content .="
+    <nav class='tabs is-centered'>
+      <ul>
+        ".$this->renderMenuLink('affiche_entreprise.php', 'Affiche entreprise')."
+        ".$this->renderMenuLink('ajout_entreprise.php', 'Ajout entreprise')."
+        ".$this->renderMenuLink('affiche_offre.php', 'Affiche offre')."
+        ".$this->renderMenuLink('ajout_offre.php', 'Ajout offre')."
+      </ul>
+    </nav>
+    <div class='container' style='padding: 1rem 1.5rem;'>
+    ";
+  }
+  function renderMenuLink($filename, $label){
+    if (basename($_SERVER["SCRIPT_FILENAME"]) == $filename){
+      // Lien actif si on est sur la page du lien
+      return "<li class='is-active'><a href='".$filename."'>".$label."</a></li>";
+    }
+    return "<li><a href='".$filename."'>".$label."</a></li>";
+  }
 
 
 
